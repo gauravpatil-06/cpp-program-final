5fe18ddbbb238a348fa803c1276c867a48da0def
Gaurav Patil <gp949958@gmail.com>
2024-02-04 12:56:22 +0530

    Implemented operator demonstrations, typecasting and basic addition program

---
+++ Basics/Operators/icrement_and_decrement_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 100, b = 200;
+
+    a++;
+    b--;
+
+    cout << "Value of a = " << a << endl;
+    cout << "Value of b = " << b;
+
+    return 0;
+}
