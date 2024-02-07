225f926572ef0bc13fc48582261c0539c16652e9
Gaurav Patil <gp949958@gmail.com>
2024-02-07 10:41:19 +0530

    Added subtraction programs

---
+++ Arithmetic/Subtraction/subtraction_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, sub;
+
+    cout << "Enter any two numbers: ";
+    cin >> num1 >> num2;
+
+    sub = num1 - num2;
+    cout << "Subtraction = " << sub << endl;
+
+    return 0;
+}
