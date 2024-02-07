5ebccaea7b8490764a9c0c8db452d94b43cd8d65
Gaurav Patil <gp949958@gmail.com>
2024-02-07 12:10:19 +0530

    Implemented multiplication table and division programs

---
+++ Arithmetic/Division/division_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, div;
+
+    cout << "Enter any two numbers: ";
+    cin >> num1 >> num2;
+
+    div = num1 / num2;
+    cout << "Division = " << div << endl;
+
+    return 0;
+}
---
+++ Arithmetic/Multiplication/simple_multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, mul;
+
+    cout << "Enter any two numbers: ";
+    cin >> a >> b;
+
+    mul = a * b;
+    cout << a << " * " << b << " = " << mul;
+
+    return 0;
+}
