375981131fb3273e190a849a1777f17d62e455bc
Gaurav Patil <gp949958@gmail.com>
2024-02-07 11:18:19 +0530

    Added multiplication programs

---
+++ Arithmetic/Multiplication/multiplication_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, mul;
+
+    cout << "Enter any two numbers: ";
+    cin >> num1 >> num2;
+
+    mul = num1 * num2;
+    cout << "Multiplication = " << mul << endl;
+
+    return 0;
+}
